3432ddb3c145c819725afa34c30277a322c35f2f
Lee-L33 <leepory2005@gmail.com>
2025-11-24 22:42:02 +0700

    get data

--- src/modules/attapue/routes.ts
+++ src/modules/attapue/routes.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { MutationServices } from "./service/mutation.service";
 import logger from "../../middlewares/logger";
-import { handleErrorOneResponse } from "../../utils";
+import { handleErrorManyResponse, handleErrorOneResponse } from "../../utils";
+import { QueryServices } from "./service/query.services";
 
 const attapueRoute = express();
 
@@ -13,6 +14,33 @@ attapueRoute.post("/create", async (req: Request, res: Response) => {
     } catch (error: any) {
         res.status(500).json(handleErrorOneResponse({
             code: "INTERNAL_SERVER_ERROR",
+            message: error.message,
+            error: {},
+        }));
+    };
+});
+
+attapueRoute.get("/get-one/:id", async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id);
+        const result = await QueryServices.get_network(id);
+        res.status(200).json(result);
+    } catch (error: any) {
+        res.status(500).json(handleErrorOneResponse({
+            code: "INTERNAL SERVER ERROR",
+            message: error.message,
+            error: {},
+        }));
+    };
+});
+
+attapueRoute.get("/get-all", async (res: Response) => {
+    try {
+        const result = await QueryServices.getAll_network();
+        res.status(200).json(result);
+    } catch (error: any) {
+        res.status(500).json(handleErrorManyResponse({
+            code: "INTERNAL SERVER ERROR",
             message: error.message,
             error: {},
         }));
--- src/modules/vientaine_pre/routes.ts
+++ src/modules/vientaine_pre/routes.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
-import { handleErrorOneResponse } from "../../utils";
+import { handleErrorManyResponse, handleErrorOneResponse } from "../../utils";
 import { MutationServices } from "./services/mutation.services";
+import { QueryServices } from "./services/query.services";
 
 const vientaine_preRoutes = express();
 
@@ -20,9 +21,24 @@ vientaine_preRoutes.post("/create", async (req: Request, res: Response) => {
 
 vientaine_preRoutes.get("/get-one/:id", async (req: Request, res: Response) => {
     try {
-        
+        const id = Number(req.params.id);
+        const result = await QueryServices.get_network(id);
+        res.status(200).json(result);
     } catch (error: any) {
         res.status(500).json(handleErrorOneResponse({
+            code: "INTERNAL SERVER ERROR",
+            message: error.message,
+            error: {},
+        }));
+    };
+});
+
+vientaine_preRoutes.get("/get-all", async (res: Response) => {
+    try {
+        const result = await QueryServices.getAll_network();
+        res.status(200).json(result);
+    } catch (error: any) {
+        res.status(500).json(handleErrorManyResponse({
             code: "INTERNAL SERVER ERROR",
             message: error.message,
             error: {},
